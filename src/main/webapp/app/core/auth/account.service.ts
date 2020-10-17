bb10eb4cfd1331cccb7aa9de0842d409aaff72c9
monju <monjurmorshed793@gmail.com>
2020-10-17 20:57:23 +0600

    auth service continued work

--- src/main/webapp/app/core/auth/account.service.ts
+++ src/main/webapp/app/core/auth/account.service.ts
@@ -1,9 +1,81 @@
 import { Injectable } from '@angular/core';
+import {Account} from "../user/account.model";
+import {Observable, of, ReplaySubject} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {StateStorageService} from "./state-storage.service";
+import {Router} from "@angular/router";
+import {SERVER_API_URL} from "../../app.constants";
+import {catchError, shareReplay, tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  private userIdentity: Account | null = null;
+  private authenticationState = new ReplaySubject<Account | null>(1);
+  private accountCache$?: Observable<Account | null>;
 
-  constructor() { }
+  constructor(private http: HttpClient, private stateStorageService: StateStorageService, private router: Router) { }
+
+  save(account: Account): Observable<{}>{
+    return this.http.post(SERVER_API_URL + 'api/account', account);
+  }
+
+  authenticate(identity: Account | null): void{
+    this.userIdentity = identity;
+    this.authenticationState.next(this.userIdentity);
+  }
+
+  hasAnyAuthority(authorities: string[] | string): boolean{
+    if(this.userIdentity || !this.userIdentity.authorities){
+      return false;
+    }
+    if(!Array.isArray(authorities)){
+      authorities = [authorities];
+    }
+    return this.userIdentity.authorities.some((authority: string)=> authorities.includes(authority));
+  }
+
+  identify(force?: boolean): Observable<Account | null>{
+    if(!this.accountCache$ || force || !this.isAuthenticated()){
+      this.accountCache$ = this.fetch().pipe(
+          catchError(()=>{
+            return of(null);
+          }),
+          tap((account: Account | null)=>{
+            this.authenticate(account);
+
+            if(account){
+              this.navigateToStoredUrl();
+            }
+          }),
+          shareReplay()
+      );
+    }
+    return this.accountCache$;
+  }
+
+  isAuthenticated(): boolean{
+    return this.userIdentity !== null;
+  }
+
+  getAuthenticationState(): Observable<Account | null>{
+    return this.authenticationState.asObservable();
+  }
+
+  getImageUrl(): string{
+    return this.userIdentity? this.userIdentity.imageUrl: '';
+  }
+
+  private fetch(): Observable<Account> {
+    return this.http.get<Account>(SERVER_API_URL + 'api/account');
+  }
+
+  private navigateToStoredUrl(): void{
+    const previousUrl = this.stateStorageService.getUrl();
+    if(previousUrl){
+      this.stateStorageService.clearUrl();
+      this.router.navigateByUrl(previousUrl);
+    }
+  }
 }
--- src/main/webapp/app/core/auth/csrf.service.ts
+++ src/main/webapp/app/core/auth/csrf.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CsrfService {
 
-  constructor() { }
+  constructor(private cookieService: CookieService) { }
+
+  getCSRF(name = 'XSRF-TOKEN'): string{
+    return this.cookieService.get(name);
+  }
 }
